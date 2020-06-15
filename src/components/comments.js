6d6476225bb115c45b3ee6bb7f072fb5c93caf04
Yomna Elnady <yomna.a.elnady@gmail.com>
2020-06-16 00:00:02 +0100

    fixing some issues

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect} from 'react';
 import {FlatList, View, Text, Button} from 'react-native';
 import {Card, ListItem, Icon} from 'react-native-elements';
 import axios from 'react-native-axios';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 const Comments = id => {
   const [comments, setComments] = useState([]);
   const [commentsFetched, setCommentsFetched] = useState(false);
   const [showComments, setShowComments] = useState(false);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     if (!commentsFetched) {
       axios
@@ -15,29 +17,36 @@ const Comments = id => {
           setComments(res.data);
           setShowComments(true);
           setCommentsFetched(true);
+          setLoading(false);
         });
     }
   }, [comments]);
   return (
-    <View
-      style={{
-        backgroundColor: '#E3F2FD',
-        justifyContent: 'space-between',
-        marginTop: 10,
-      }}>
-      <FlatList
-        data={comments}
-        renderItem={({item}) => (
-          <View>
-            <Card title={item.name.toUpperCase()}>
-              <View style={{justifyContent: 'space-between'}}>
-                <Text style={{margin: 10}}>{item.body}</Text>
+    <View>
+      {loading ? (
+        <Spinner visible={loading} textContent={'Loading...'} size="small" />
+      ) : (
+        <View
+          style={{
+            backgroundColor: '#E3F2FD',
+            justifyContent: 'space-between',
+            marginTop: 10,
+          }}>
+          <FlatList
+            data={comments}
+            renderItem={({item}) => (
+              <View>
+                <Card title={item.name.toUpperCase()}>
+                  <View style={{justifyContent: 'space-between'}}>
+                    <Text style={{margin: 10}}>{item.body}</Text>
+                  </View>
+                </Card>
               </View>
-            </Card>
-          </View>
-        )}
-        keyExtractor={item => item.id}
-      />
+            )}
+            keyExtractor={item => item.id}
+          />
+        </View>
+      )}
     </View>
   );
 };
--- src/components/post.js
+++ src/components/post.js
@@ -7,17 +7,21 @@ const Post = posts => {
   const [showComments, setShowComments] = useState(false);
   const [postId, setPostId] = useState(0);
   const [open, setOpen] = useState(false);
-  const [currentPosts, setCurrentPosts] = useState(posts.posts.slice(0, 2));
+  const [currentPosts, setCurrentPosts] = useState(posts.posts.slice(0, 3));
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(2);
+  const [postsPerPage, setPostsPerPage] = useState(3);
 
   const onPressLearnMore = () => {
+    console.log(55);
+
     setCurrentPage(currentPage + 1);
 
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
 
-    setCurrentPosts(posts.posts.slice(indexOfFirstPost, indexOfLastPost));
+    setCurrentPosts(
+      currentPosts.concat(posts.posts.slice(indexOfFirstPost, indexOfLastPost)),
+    );
   };
 
   return (
@@ -27,37 +31,38 @@ const Post = posts => {
         justifyContent: 'space-between',
         marginTop: 10,
       }}>
-      <Button
-        onPress={onPressLearnMore}
-        title="Read More"
-        color="#BBDEFB"
-        style={{marginBottom: 20}}
-      />
       <FlatList
         data={currentPosts}
-        renderItem={({item}) => (
-          <View>
-            <Card title={item.title.toUpperCase()}>
-              <View style={{justifyContent: 'space-between'}}>
-                <Text style={{margin: 10}}>{item.body}</Text>
-                <Button
-                  onPress={() => {
-                    setPostId(item.id);
-                    setShowComments(!showComments);
-                  }}
-                  title="Comments"
-                  color="#BBDEFB"
-                />
-                {showComments && item.id == postId ? (
-                  <Comments postId={postId} />
-                ) : (
-                  <View></View>
-                )}
-              </View>
-            </Card>
-          </View>
-        )}
         keyExtractor={item => item.id}
+        renderItem={({item}) => {
+          console.log(item);
+
+          return (
+            <View>
+              <Card title={item.title.toUpperCase()}>
+                <View style={{justifyContent: 'space-between'}}>
+                  <Text style={{margin: 10}}>{item.body}</Text>
+                  <Button
+                    onPress={() => {
+                      setPostId(item.id);
+                      setShowComments(!showComments);
+                    }}
+                    title="Comments"
+                    color="#BBDEFB"
+                  />
+                  {showComments && item.id == postId ? (
+                    <Comments postId={postId} />
+                  ) : (
+                    <View></View>
+                  )}
+                </View>
+              </Card>
+            </View>
+          );
+        }}
+        onEndReachedThreshold={0.1}
+        onEndReached={onPressLearnMore}
+        scrollEventThrottle={400}
       />
     </View>
   );
